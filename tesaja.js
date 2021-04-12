56e66772e75e79ba1ae77b9b6cba70583efec4cd
taruna <taruna.rachmad@gmail.com>
2021-04-12 17:46:16 +0700

    first commit

---
+++ tesaja.js
@@ -0,0 +1,11 @@
+a = [{
+    title: 'd',
+    tags: [ 'dfa' ],
+    body: '',
+    id: 'dDo5sXqVGTTX6wQz',
+    createdAt: '2021-04-12T10:12:22.450Z',
+    updatedAt: '2021-04-12T10:12:22.450Z'
+  }]
+
+
+console.log(a[0])
